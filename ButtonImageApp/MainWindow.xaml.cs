ee2a3bb6cfb8a6f84e9990c5fd665c527726f4ef
andreimalvich <andreimalvich@gmail.com>
2026-03-16 14:07:51 +0300

    rtyhfg

--- ButtonImageApp/MainWindow.xaml.cs
+++ ButtonImageApp/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace ButtonImageApp
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
